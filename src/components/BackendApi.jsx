a4e6f6d477839113276971b27460e3a60f19bbc7
vplatonov <vplatonovmail@gmail.com>
2024-11-20 19:09:33 -0500

    Login/register API

--- src/components/BackendApi.jsx
+++ src/components/BackendApi.jsx
@@ -2,6 +2,6 @@ import {ApiClient, DefaultApi} from "../../api-client/src";
 
 export function initBackendApi() {
     const apiClient = new ApiClient();
-    apiClient.basePath = `http://localhost:8080/api/v2`;
+    apiClient.basePath = `http://143.198.40.227:8080/api/v2`;
     return new DefaultApi(apiClient);
 }
