7d9db02a105d2c464dd52c79f71d476d1817815f
Alessya Krapivina <alessya.k00@gmail.com>
2024-12-11 16:55:18 -0500

    fixed pet card's image to fit properly

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,72 +1,69 @@
-import React, {useState} from "react";
-import {Card, Button, Row, Col, Image, Modal} from 'react-bootstrap';
+import React, { useState } from "react";
+import { Card, Button, Row, Col, Image, Modal } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import PetProfile from "./PetProfile";
 
 const PetCard = ({ pet, imageSrc, attributeGroups }) => {
+  const [showModal, setShowModal] = useState(false);
 
-    const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleOpenModal = () => setShowModal(true);
 
-    const handleOpenModal = () => setShowModal(true);
-
-    const handleAdoptionClick = () => {
-        navigate('/adoption');
-    };
+  const handleAdoptionClick = () => {
+    navigate("/adoption");
+  };
 
   return (
     <Col md={4}>
       <Card className="mb-4 shadow-sm">
-        <Card.Title>{pet.petName}</Card.Title>
-        <Card.Img src={imageSrc}
-                  alt="Pet's Photo"
-                  className="rounded-circle"
-                  style={{
-                    width: "400px",
-                    height: "400px",
-                    margin: "0 40px",
-                    objectFit: "cover",
-                  }} />
+        <Card.Title className="mt-3 mb-3">{pet.petName}</Card.Title>
+        <Card.Img
+          src={imageSrc}
+          alt="Pet's Photo"
+          className="rounded-circle"
+          style={{
+            width: "300px",
+            height: "300px",
+            margin: "0 auto",
+            objectFit: "cover",
+            borderRadius: "50%",
+          }}
+        />
         <Card.Body>
           <Row>
             <Col>
-              <Button
-                  onClick={handleOpenModal}
-                  variant="success"
-              >
+              <Button onClick={handleOpenModal} variant="success">
                 View pet’s profile
               </Button>
-              <br/><br/>
+              <br />
+              <br />
             </Col>
           </Row>
           <Row>
             <Col>
-              <Button variant="success"
-                      onClick={handleAdoptionClick}
-              >
-                  {pet.status === 'adopted' ? 'Adopted' : 'Still available'}
+              <Button variant="success" onClick={handleAdoptionClick}>
+                {pet.status === "adopted" ? "Adopted" : "Still available"}
               </Button>
             </Col>
           </Row>
         </Card.Body>
       </Card>
 
-        {/* Modal for Adding New Pet Profile */}
-        <Modal show={showModal} onHide={() => setShowModal(false)} size="xl">
-            <Modal.Header closeButton>
-                <Modal.Title>Pet Profile View: {pet.petName}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <PetProfile
-                    attributeGroups={attributeGroups}
-                    pet={pet}
-                    imageSrc={imageSrc}
-                />
-            </Modal.Body>
-        </Modal>
-
-</Col>
+      {/* Modal for Adding New Pet Profile */}
+      <Modal show={showModal} onHide={() => setShowModal(false)} size="xl">
+        <Modal.Header closeButton>
+          <Modal.Title>Pet Profile View: {pet.petName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <PetProfile
+            attributeGroups={attributeGroups}
+            pet={pet}
+            imageSrc={imageSrc}
+          />
+        </Modal.Body>
+      </Modal>
+    </Col>
   );
 };
 
